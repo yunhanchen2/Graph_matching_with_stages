e30963dca1a018d21a0e65e0b134dd5ee899b751
yunhanchen2 <135736371+yunhanchen2@users.noreply.github.com>
2024-06-19 19:24:22 +0800

    Update CSRGraph.cpp

--- src/CSRGraph.cpp
+++ src/CSRGraph.cpp
@@ -377,11 +377,11 @@ void CSRGraph::Generate_stages(){
     cout<<"testing"<<endl;
 
 
-    //delete the new array
-    for (int i = 0; i < change_of_edge*2; ++i) {
-        delete [] array_changing[i];
-    }
-    delete [] array_changing;
+    // //delete the new array
+    // for (int i = 0; i < change_of_edge*2; ++i) {
+    //     delete [] array_changing[i];
+    // }
+    // delete [] array_changing;
 
     //testing
     cout<<"testing 1"<<endl;
